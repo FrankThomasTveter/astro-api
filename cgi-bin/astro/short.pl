b29c5a12565013a880de49961285012f68913385
Geir Aalberg <geira@met.no>
2019-03-25 13:13:28 +0100

    ported post-processing from api to astro

--- cgi-bin/astro/short.pl
+++ cgi-bin/astro/short.pl
@@ -5,120 +5,100 @@ use strict;
 
 use XML::LibXSLT;
 use XML::LibXML;
+use DateTime;
+use Apache2::Request;
 
-my $xsl= << EOF;
-<?xml version="1.0" encoding="utf-8"?>
+# sudo apt-get install libdatetime-perl libapache2-request-perl
 
-<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0"
-                xmlns:astro="http://astro.met.no" exclude-result-prefixes="astro">
-  <xsl:output method="xml" indent="yes" encoding="utf-8"/>
+my $maxdays = 15;
+my $xsl= '/var/www/cgi-bin/astro/short.xsl';
 
-  <xsl:template match="/*">
-    <astrodata xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-               xsi:noNamespaceSchemaLocation="http://schema.api.met.no/schemas/astrodata-2.0.xsd">
-      <meta licenseurl="https://api.met.no/license_data.html"/>
-        <location latitude="{astro:Event[1]/@Val1}"
-                  longitude="{/*/astro:Event[1]/@Val2}"
-                  height="{/*/astro:Event[1]/@Val3}">
-          <xsl:apply-templates select="/*/astro:Event[@Id='100']"/>
-          <xsl:apply-templates select="/*/astro:Event[@Id='110']"/>
-          <xsl:apply-templates select="/*/astro:Event/astro:Report"><!-- this will match above once more, but since is empty will result in null string -->
-            <xsl:sort select="@time"/>
-          </xsl:apply-templates>
-        </location>
-    </astrodata>
-  </xsl:template>
+my $req = Apache2::Request->new();
 
-  <!-- sun stuff -->
-  <xsl:template match="/*/astro:Event[@Id='600']/astro:Report">
-    <sunrise time="{@time}" desc="{substring(@hint,21)}"/>
-  </xsl:template>
+# calculate time params
 
-  <xsl:template match="/*/astro:Event[@Id='610']/astro:Report">
-    <sunset time="{@time}" desc="{substring(@hint,21)}"/>
-  </xsl:template>
+my ($offset) = $req->param('offset') =~ /^([ +-]?\d\d:\d\d)$/a
+    or die "Error in offset format: " . $req->param('offset');
+$offset =~ s/^( |)(?=\d)/+/; # + is escaped as space or missing, put it back
+my $days = $req->param('days')||1;
 
-  <xsl:template match="/*/astro:Event[@Id='620']/astro:Report">
-    <solarnoon time="{@time}" elevation="{@repVal}" desc="{substring(@hint,21)}"/>
-  </xsl:template>
+my $from = DateTime->new(
+    year => $1,
+    month => $2,
+    day => $3,
+    time_zone  => $offset,
+) if $req->param('date') =~ /(\d\d\d\d)-(\d\d)-(\d\d)/a; # disable unicode numbers
+die 'Illegal date format: ' . $req->param('date') unless $from;
+$from->set_time_zone('UTC');
+my $to = $from->clone->add( days => $days);
 
-  <xsl:template match="/*/astro:Event[@Id='630']/astro:Report">
-    <solarmidnight time="{@time}" elevation="{@repVal}" desc="{substring(@hint,21)}"/>
-  </xsl:template>
+# generate astro query
 
-  <!-- moon stuff -->
-  <xsl:template match="/*/astro:Event[@Id='100']">
-    <moonphase time="{astro:Report[@repId='102']/@time}"
-               value="{astro:Report[@repId='102']/@repVal}"
-               desc="{substring(astro:Report[@repId='102']/@hint,21)}"/>
-    <moonshadow time="{astro:Report[@repId='103']/@time}"
-                elevation="{astro:Report[@repId='104']/@repVal}"
-                azimuth="{astro:Report[@repId='103']/@repVal}"
-                desc="{substring(astro:Report[@repId='103']/@hint,21)}"/>
-  </xsl:template>
+my @events = (0, 100, 110, 210, 220, 230, 240, 600, 610, 620, 630, 800, 810, 820, 830, 900, 910, 920, 940);
+my $query = sprintf "eventStart=%sZ&eventStop=%sZ&eventSearch=2&eventVal1=%f&eventVal2=%f&eventVal3=%f",
+    $from->iso8601, $to->iso8601, $req->param('lat'), $req->param('lon'), $req->param('alt')||0;
+$query .= "&eventVal4=1"; # get moonposition for each day + 1 extra (bug?)
+$query .= "&event${_}Id=".$events[$_] for 1..$#events;
+#print STDERR "(Sunrise) Fetching $query\n";
 
-  <xsl:template match="/*/astro:Event[@Id='110']">
-    <moonposition time="{child::astro:Report[@repId='110']/@time}"
-                  elevation="{child::astro:Report[@repId='110']/@repVal}"
-                  azimuth="{child::astro:Report[@repId='111']/@repVal}"
-                  range="{child::astro:Report[@repId='112']/@repVal}"
-                  desc="{substring(child::astro:Report[@repId='110']/@hint,21)}"/>
-  </xsl:template>
+$ENV{'QUERY_STRING'} = $query;
 
-  <xsl:template match="/*/astro:Event[@Id='800']/astro:Report">
-    <moonrise time="{@time}" desc="{substring(@hint,21)}"/>
-  </xsl:template>
-
-  <xsl:template match="/*/astro:Event[@Id='810']/astro:Report">
-    <moonset time="{@time}" desc="{substring(@hint,21)}"/>
-  </xsl:template>
-
-  <xsl:template match="/*/astro:Event[@Id='820']/astro:Report">
-    <high_moon time="{@time}" elevation="{@repVal}" desc="{substring(@hint,21)}"/>
-  </xsl:template>
-
-  <xsl:template match="/*/astro:Event[@Id='830']/astro:Report">
-    <low_moon time="{@time}" elevation="{@repVal}" desc="{substring(@hint,21)}"/>
-  </xsl:template>
-
-  <!-- polar stuff -->
-  <xsl:template match="/*/astro:Event[@Id='900']/astro:Report">
-    <polardaystart time="{@time}" desc="{substring(@hint,21)}"/>
-  </xsl:template>
-
-  <xsl:template match="/*/astro:Event[@Id='910']/astro:Report">
-    <polardayend time="{@time}" desc="{substring(@hint,21)}"/>
-  </xsl:template>
-
-  <xsl:template match="/*/astro:Event[@Id='920']/astro:Report">
-    <polarnightstart time="{@time}" desc="{substring(@hint,21)}"/>
-  </xsl:template>
-
-  <xsl:template match="/*/astro:Event[@Id='930']/astro:Report">
-    <polarnightend time="{@time}" desc="{substring(@hint,21)}"/>
-  </xsl:template>
-
-</xsl:stylesheet>
-EOF
-
+# fetch XML
 
 my @lines = &Astro::Api::event();
 
+# Print HTTP headers (should be done in this wrapper)
+
 print shift(@lines) . "\n";
 print shift(@lines) . "\n";
 
+# transform XML and output result
 
-my $source = XML::LibXML->load_xml(string => join "\n", @lines);
-
-my $style_doc = XML::LibXML->load_xml(string=>$xsl,no_cdata=>1);
+my $parser = XML::LibXML->new(no_cdata=>1);
+my $source = $parser->parse_string(join "\n", @lines);
+my $style_doc = $parser->parse_file($xsl);
 my $stylesheet = XML::LibXSLT->new->parse_stylesheet($style_doc);
 
-my $results = $stylesheet->transform($source);
+my $temp = $stylesheet->transform($source);
+my $results = postproc( $temp, $offset );
 print $stylesheet->output_as_bytes($results);
 
 #
-my @lines = &Astro::Api::event();
-foreach my $line (@lines) {
-    print "$line\n";
-}
+#my @lines = &Astro::Api::event();
+#foreach my $line (@lines) {
+#    print "$line\n";
+#}
 #
+
+sub postproc { # sort XML by date and localize times
+    my ($dom, $offset) = @_;
+    my ($topnode) = $dom->findnodes('/astrodata/location');
+    my %dates;
+    for my $node ( $topnode->childNodes ) {
+        next unless $node->nodeType == XML_ELEMENT_NODE;
+        my @keys = qw( year month day hour minute second );
+        my $utctime = $node->getAttribute('time');
+        my %bits;
+        @bits{@keys} = $utctime =~ /^(\d\d\d\d)-(\d\d)-(\d\d)T(\d\d):(\d\d):(\d\d)Z/ or next; # should not happen
+        # workaround since DateTime can't parse T24:00 from astro
+        my $hours = delete $bits{'hour'};
+        my $localtime = DateTime->new( %bits, time_zone  => 'UTC' );
+        $localtime->add( hours => $hours ); # add hours back on
+        $localtime->set_time_zone($offset);
+        # cut out node from doc and file by date
+        $topnode->removeChild( $node )->setAttribute('time', $localtime . $offset);
+        my $date = $localtime->ymd;
+        if (exists $dates{$date} ) {
+            push @{ $dates{$date} }, $node;
+        } else {
+            $dates{$date} = [ $node ];
+        }
+    }
+    # add nodes grouped by date element
+    for my $date (sort keys %dates) {
+        my $datenode = $topnode->addNewChild('', 'time');
+        $datenode->setAttribute('date', $date);
+        $datenode->appendChild($_) for @{ $dates{$date} };
+    }
+    return $dom;
+}
