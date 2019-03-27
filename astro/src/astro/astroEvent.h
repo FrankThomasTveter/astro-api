0158c00a89277f3d775737f515d4c4622d032d58
FrankThomasTveter <f.t.tveter@gmail.com>
2019-03-27 13:37:23 +0100

    Added short_ in xs-header.

--- astro/src/astro/astroEvent.h
+++ astro/src/astro/astroEvent.h
@@ -15,6 +15,7 @@ void jplephclose_();
 void jd2date_(double* JD, int* year, int* month, int* mday, int* hour, int* min, double* secs);
 void date2jd_(double* JD, int* year, int* month, int* mday, int* hour, int* min, double* secs);
   
+void short_();
 void event_();
 void state_();
 void astroevent_( double *tstart2000, // start time (in jd2000)
